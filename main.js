b89e61287666d473da1277e0953443c4c7b1677b
dayuss <ddtardian@gmail.com>
2022-11-05 14:46:20 +0700

    fix production

--- main.js
+++ main.js
@@ -26,6 +26,6 @@ app.use(express.json())
 app.use(router);
 app.use(errorResponder)
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
