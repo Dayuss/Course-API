da674d9a66244dbeff0b2addfb7947727bbfb442
dayuss <ddtardian@gmail.com>
2022-11-05 14:28:57 +0700

    fix production

--- config/config.js
+++ config/config.js
@@ -16,10 +16,16 @@ module.exports = {
         dialect: process.env.DIALECT
     },
     production: {
-        username: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DBNAME,
-        host: process.env.HOST,
-        dialect: process.env.DIALECT
+        "username": "zxhhfrwnslvgcw",
+        "password": "2e1963d3d0c1e4a6ddb75d395aea78920ca9a4539951b7d7fa6aafcf384733b9",
+        "database": "d3mjoog1rcso0r",
+        "host": "ec2-44-195-132-31.compute-1.amazonaws.com",
+        "dialect": "postgres",
+        "dialectOptions": {
+                "ssl": {
+                    "require": true,
+                    "rejectUnauthorized": false
+                }
+        }
     }
 };
